573c92615704355c39d646f6d2c9a6ba9d8df4bc
poly <mcx_221@foxmail.com>
2013-12-04 19:50:39 +0800

    Read txt attachment

--- gmail/attachreader.py
+++ gmail/attachreader.py
@@ -1,35 +1,45 @@
-from subprocess import Popen, PIPE, STDOUT
+from subprocess import Popen, PIPE, STDOUT, CalledProcessError
 from bson import Binary
 
 def read(fcontent, fname):
+    if isinstance(fcontent, unicode):
+        return fcontent
     try:
         return _read(fcontent, fname)
     except Exception as e:
         # need a logger
-        print e
+        print '+'*20, e
         try:
-            return unicode(fcontent)
-        except Exception as e:
-            print e
-            return Binary(fcontent)
+            # if left off, ascii will be the default encoding
+            return unicode(fcontent, 'gbk') 
+        except UnicodeDecodeError:
+            try:
+                return unicode(fcontent, 'utf-8')
+            except Exception as e:
+                print e
+                return Binary(fcontent)
 
 #TODO refine your shit
 def _read(fcontent, fname):
     if fname.endswith(('doc', 'docx')):
-        # Don't redirect stderr to a PIPE when you're not reading it.
-        p = Popen('catdoc', stdin=PIPE, stdout=PIPE, stderr=STDOUT)
-        p.stdin.write(fcontent)
-        p.stdin.close()
-        return p.stdout.read()
+        return pread('catdoc', fcontent)
     elif fname.endswith('pdf'):
+        return pread(['pdftotext', '-', '-'], fcontent)
+    raise Exception('Not any of them')
+
+def pread(cmd, input):
         # Don't redirect stderr to a PIPE when you're not reading it.
-        p = Popen(['pdftotext', '-', '-'], stdin=PIPE, stdout=PIPE, stderr=STDOUT)
-        p.stdin.write(fcontent)
-        p.stdin.close()
-        return p.stdout.read()
-    return fc
+        proc = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+        # proc.stdin.write(fcontent)
+        # proc.stdin.close()
+        # return proc.stdout.read()
+        output, unused_err = proc.communicate(input)
+        retcode = proc.returncode
+        if retcode:
+            raise CalledProcessError(retcode, cmd, output)
+        return output
 
 if __name__ == '__main__':
-    fn = '/tmp/t.pdf'
+    fn = '/tmp/goo.txt'
     fc = open(fn, 'rb').read()
     print read(fc, fn)
