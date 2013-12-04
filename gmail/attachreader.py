ddcdd37014425cd5b3db075f22a2f41602e6fa0b
poly <mcx_221@foxmail.com>
2013-12-04 11:32:38 +0800

    An ad-hoc pdf reader

--- gmail/attachreader.py
+++ gmail/attachreader.py
@@ -1,16 +1,35 @@
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, STDOUT
+from bson import Binary
+
+def read(fcontent, fname):
+    try:
+        return _read(fcontent, fname)
+    except Exception as e:
+        # need a logger
+        print e
+        try:
+            return unicode(fcontent)
+        except Exception as e:
+            print e
+            return Binary(fcontent)
 
 #TODO refine your shit
-def read(fc, fname):
+def _read(fcontent, fname):
     if fname.endswith(('doc', 'docx')):
-        # stderr?
-        p = Popen('catdoc', stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        p.stdin.write(fc)
+        # Don't redirect stderr to a PIPE when you're not reading it.
+        p = Popen('catdoc', stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+        p.stdin.write(fcontent)
+        p.stdin.close()
+        return p.stdout.read()
+    elif fname.endswith('pdf'):
+        # Don't redirect stderr to a PIPE when you're not reading it.
+        p = Popen(['pdftotext', '-', '-'], stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+        p.stdin.write(fcontent)
         p.stdin.close()
         return p.stdout.read()
     return fc
 
 if __name__ == '__main__':
-    fn = '/tmp/test.doc'
+    fn = '/tmp/t.pdf'
     fc = open(fn, 'rb').read()
     print read(fc, fn)
