c2f84e9ca33e29acde470ec3c73a8c34caf0d82d
poly <mcx_221@foxmail.com>
2013-12-14 03:54:42 +0800

    Adjust some logic

--- gmail/utils.py
+++ gmail/utils.py
@@ -1,13 +1,20 @@
-def decode_str(s, encodings=('gbk', 'utf-8'), errors='strict', E=UnicodeDecodeError):
+def decode_str(s, encodings=('gbk', 'utf-8'), E=UnicodeDecodeError):
     """Try to decode a string in different ways(encodings), 
     raise a specific error(E) when decoding fails
     """
     if isinstance(s, unicode):
         return s
-    for encoding in encodings:
+    if isinstance(encodings, tuple):
+        for encoding in encodings:
+            try:
+                return s.decode(encoding)
+            except UnicodeDecodeError:
+                pass
+        raise E("'%s' cannot be decoded by any of %s" % (s, encodings))
+    else:
         try:
-            return s.decode(encoding, errors)
+            return s.decode(encodings)
         except UnicodeDecodeError:
             pass
-    raise E("'%s' cannot be decoded by any of %s" % (s, encodings))
+        raise E("'%s' cannot be decoded by %s" % (s, encodings))
 
