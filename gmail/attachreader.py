e9fcec18ca639d030b2ad40b3ef40d91322816a6
poly <mcx_221@foxmail.com>
2013-12-16 21:12:05 +0800

    Excel

--- gmail/attachreader.py
+++ gmail/attachreader.py
@@ -1,6 +1,7 @@
 import logging
 from subprocess import Popen, PIPE, STDOUT, CalledProcessError
 
+from xlrd import open_workbook
 from utils import decode_str
 
 logger = logging.getLogger(__name__)
@@ -24,6 +25,8 @@ def _read(fcontent, fname):
         return pread('catdoc', fcontent)
     elif fname.endswith('pdf'):
         return pread(['pdftotext', '-', '-'], fcontent)
+    elif fname.endswith(('xls', 'xlsx')):
+        return read_xls(fcontent)
     raise Exception('Not any of them')
 
 def pread(cmd, input):
@@ -37,4 +40,18 @@ def pread(cmd, input):
         if retcode:
             raise CalledProcessError(retcode, cmd, output)
         return output
+
+def read_xls(fcontent):
+    values = []
+    # encoding_override
+    # Used to overcome missing or bad codepage information in older-version files.
+    # From Excel 97 onwards, unicode is default, no worries
+    wb = open_workbook(file_contents=fcontent, encoding_override='gbk')
+    for sheet in wb.sheets():
+        for irow in range(sheet.nrows):
+            for icol in range(sheet.ncols):
+                cell = sheet.cell(irow, icol)
+                if cell.ctype:
+                    values.append(unicode(cell.value))
+    return '\t'.join(values)
 
