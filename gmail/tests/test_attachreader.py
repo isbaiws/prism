e35863eda22a52f391e4d2cc55aaac21ddeb60ae
poly <mcx_221@foxmail.com>
2014-01-08 19:19:26 +0800

    Tell what's wrong when attachreader fails

--- gmail/tests/test_attachreader.py
+++ gmail/tests/test_attachreader.py
@@ -8,6 +8,8 @@ fn_doc = 'fixtures/ping.doc'
 fn_pdf = 'fixtures/ping.pdf'
 
 class AttachReaderTestCase(TestCase):
+    longMessage = True
+
     def test_txt_reader(self):
         res = attachreader.read('我的', 'a.txt')
         self.assertIsInstance(res, unicode)
@@ -15,11 +17,11 @@ class AttachReaderTestCase(TestCase):
 
     def test_doc_reader(self):
         res = attachreader.read(self.read_binary(fn_doc), fn_doc)
-        self.assertEqual(res, 'ping')
+        self.assertEqual(res, 'ping', 'Cannot read MS-Word, try install antiword.')
 
     def test_pdf_reader(self):
         res = attachreader.read(self.read_binary(fn_pdf), fn_pdf)
-        self.assertEqual(res, 'ping')
+        self.assertEqual(res, 'ping', 'Cannot read pdf, try install xpdf.')
 
     def read_binary(self, fn):
         with open(os.path.join(os.path.dirname(__file__), fn),
