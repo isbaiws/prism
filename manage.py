1468126681f7565d1ce378dddd2e80a645572bcd
poly <mcx_221@foxmail.com>
2014-01-14 00:50:25 +0800

    Send mail with command `./manage.py sendmail`

--- manage.py
+++ manage.py
@@ -14,5 +14,36 @@ if __name__ == "__main__":
         import prism.settings
         from mongoengine.connection import get_connection
         get_connection().drop_database('prism')
+    elif sys.argv[1] == 'sendmail':
+        import requests
+        login_url = 'http://localhost:8000/login'
+        post_email_url = 'http://localhost:8000/email'
+        payload = {
+            'username': 'test',
+            'password': 'test',
+        }
+
+        def pretty_header(r):
+            print '\n'.join('< %s: %s' % (k, v) for k, v in r.request.headers.items())
+            print
+            print '\n'.join('> %s: %s' % (k, v) for k, v in r.headers.items())
+            print
+
+        s = requests.Session()
+        r = s.post(login_url, data=payload, allow_redirects=False)
+        # pretty_header(r)
+        if r.status_code != 302:
+            print 'Login failed!!'
+            sys.exit(1)
+
+        if len(sys.argv) < 3:
+            print 'Specify a file path!!'
+            sys.exit(1)
+        fn = sys.argv[2]
+        # files = {'file': open(fn, 'rb')}
+        r = s.post(post_email_url, data=open(fn, 'rb'))
+        # pretty_header(r)
+        # print r.json()
+        print r.text
     else:
         execute_from_command_line(sys.argv)
