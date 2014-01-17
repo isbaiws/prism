92e939fc9b336f110a505cf7ed8d43ac250207af
poly <mcx_221@foxmail.com>
2014-01-17 20:56:59 +0800

    Fixed some user_add links

--- prism/urls.py
+++ prism/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     url(r'^logout$', views.Logout.as_view(), name='logout'),
 
     url(r'^user$', views.UserDetail.as_view(), name='user_detail'),
-    url(r'^user/add$', views.AddUser.as_view(), name='add_user'),
+    url(r'^user/add$', views.AddUser.as_view(), name='user_add'),
 
     # url(r'^email$', views.EmailList.as_view(), name='email_list'),
     url(r'^email/path$', views.EmailList.as_view(), name='email_list'),
