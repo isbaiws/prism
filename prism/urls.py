3d1b27f766c9113a56ac3131df64fb696f1c31f0
poly <mcx_221@foxmail.com>
2014-03-30 17:03:27 +0000

    Set email list as home page

--- prism/urls.py
+++ prism/urls.py
@@ -6,7 +6,8 @@ from gmail import views
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', views.Index.as_view(), name='index'),
+    # url(r'^$', views.Index.as_view(), name='index'),
+    url(r'^$', views.EmailList.as_view(), name='index'),
 
     url(r'^login$', views.Login.as_view(), name='login'),
     url(r'^logout$', views.Logout.as_view(), name='logout'),
